2d85d3834fab709b023ab27d3f90bfff40e7ef44
akash6sharma <akash6.sharma@paytm.com>
2020-07-12 05:19:17 +0530

    hystrix dashboard changes

--- src/main/java/com/akash/context/AppContext.java
+++ src/main/java/com/akash/context/AppContext.java
@@ -2,5 +2,5 @@ package com.akash.context;
 
 public class AppContext {
 
-  public static int HTTP_STATUS_SERIES = 200; // 200, 400 , 500 , any other number (time-out)
+  public static int HTTP_STATUS_SERIES = 500; // 200, 400 , 500 , any other number (time-out)
 }
